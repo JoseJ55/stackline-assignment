a01f42316010e5a23eb44a04be05b74be8fc3fdb
JoseJ55 <josejimenez826@gmail.com>
2024-04-13 12:34:08 -0700

    added the graph component"

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,59 +1,105 @@
 import './Graph.css';
 import React from 'react'
 
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { Line } from 'react-chartjs-2';
+import {
+    Chart,
+    defaults,
+    registerables
+} from 'chart.js';
 
 import { useAppSelector } from '../../app/hooks';
 
-interface salesStructure {
-    weekEnding: string,
-    retailSales: number,
-    wholesaleSales: number,
-    unitsSold: number,
-    retailerMargin: number
-}
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
 
-const transformData = (data: salesStructure[]) => {
-    const monthlySales: { [month: string]: number } = {};
-    data.forEach(item => {
-      const month = item.weekEnding.split('-')[1];
-      monthlySales[month] = (monthlySales[month] || 0) + item.retailSales;
-    });
-  
-    return Object.keys(monthlySales).map(month => ({
-      name: month,
-      retailSales: monthlySales[month]
-    }));
-};
+Chart.register(...registerables)
 
 export default function Graph() {
     const { currentProduct } = useAppSelector((state) => state.product);
+
+    const retailSalesData = currentProduct?.sales.map((sale) => sale?.retailSales);
+    const wholesaleSalesData = currentProduct?.sales.map((sale) => sale?.wholesaleSales);
+    const labels = currentProduct?.sales.map((sale) => sale?.weekEnding);
     
-    // const transformedData = transformData(currentProduct?.sales || []);
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'Retail Sales',
+                data: retailSalesData,
+                backgroundColor: 'transparent',
+                borderColor: '#44a6f6',
+                pointBorderColor: '#44a6f6',
+                pointStyle: 'line',
+                pointRadius: 0,
+                fill: false,
+                tension: 0.4
+            },
+            {
+                label: 'Wholesale Sales',
+                data: wholesaleSalesData,
+                backgroundColor: 'transparent',
+                borderColor: '#98a3bd',
+                pointBorderColor: '#98a3bd',
+                pointStyle: 'line',
+                pointRadius: 0,
+                fill: false,
+                tension: 0.4
+                }
+        ]
+    }
+
+    const maxValue = Math.max(...retailSalesData ?? [], ...wholesaleSalesData ?? []);
+
+    const options = {
+        scales: {
+            x: {
+                display: false,
+                grid: {
+                    display: false
+                },
+            },
+            y: {
+                display: false,
+                min: -maxValue,
+                max: maxValue * 2
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            tooltip: {
+                intersect: false,
+            }
+        },
+    }
 
     return (
         <div id='graph'>
             <h3 id='graph-title'>Retail Sales</h3>
             <div id='graph-container'>
-            {/* <LineChart
-                width={100}
-                height={400}
-                data={transformedData}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="retailSales" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="retailSales" stroke="#82ca9d" />
-            </LineChart> */}
+                <Line
+                    data={data}
+                    options={options}
+                />
+                <div id='graph-labels'>
+                    <ul>
+                        <li>JAN</li>
+                        <li>FEB</li>
+                        <li>MAR</li>
+                        <li>APR</li>
+                        <li>MAY</li>
+                        <li>JUN</li>
+                        <li>JUL</li>
+                        <li>AUG</li>
+                        <li>SEP</li>
+                        <li>OCT</li>
+                        <li>NOV</li>
+                        <li>DEC</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
