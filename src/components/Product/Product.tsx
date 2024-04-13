53c3a51834c6b0dfced746ec4469514fc8bbd8f0
JoseJ55 <josejimenez826@gmail.com>
2024-04-12 20:05:15 -0700

    adde the detials section

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -14,8 +14,17 @@ export default function Product() {
     }, [allProducts, currentProduct, isLoading, isError]);
 
     return (
-        <div>
-            product
+        <div id='product'>
+            <img id='product-image' src={currentProduct?.image} alt={`image of the product ${currentProduct?.title}`} />
+
+            <h1 id='product-title'>{currentProduct?.title}</h1>
+            <h2 id='product-subtitle'>{currentProduct?.subtitle}</h2>
+
+            <div id='product-tags'>
+                {currentProduct?.tags.map((tag) => (
+                    <p id='product-tag'>{tag}</p>
+                ))}
+            </div>
         </div>
     )
 }
